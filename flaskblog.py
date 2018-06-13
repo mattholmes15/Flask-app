27468f39ec8c7cb6dc2cd429ab652a7e1a3008da
Matt <mholmes01@btinternet.com>
2018-06-13 10:54:55 +0100

    Started DB coding, added JSON learning

--- flaskblog.py
+++ flaskblog.py
@@ -6,31 +6,36 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite//site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 #DB info here
 
-#class User(db.Model):
-#    id = db.column(db.Integer, primary_key)
-#    username = db.column(db.String(20), unique=True, nullable=False)
-#    email = db.column(db.String(120), unique=True, nullable=False)
-#    image_file = db.column(db.String(20)), nullable=False, default='default.jpg')
-#    password = db.column(db.String(60)), nullable=False)
-#
-##Repr how the object is printed out
-#
-#    def __repr__(self):
-#        return f"User('{self.username}'), '{self.email}', '{self.image_file}')"
-#
-#class Post(db.Model):
-#    id = db.column(db.Integer, primary key)
-#    title = db.column(db.String(100), nullable=False)
-#    date = db.column(db.DateTime, nullable=False, default=datetime.utcnow)
-#    content = db.column(db.Text, nullable=False)
-#
-#def __repr__(self):
-#    return f"Post('{self.title}'), '{self.date_posted}')"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    #Athor attribute to get the user who created the post
+    #Lazy defines when SQLAlchemy loads the data - lazy will load the data in one go
+
+#Repr how the object is printed out
+
+    def __repr__(self):
+        return f"User('{self.username}'), '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+def __repr__(self):
+    return f"Post('{self.title}', '{self.date_posted}')"
 
 
 
